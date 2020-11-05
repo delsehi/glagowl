82f6e5994f03311cc03e15bca634747ad252d1d9
Delfi Sehidic <ds222qe@student.lnu.se>
2020-11-05 23:58:49 +0100

    Basic html setup

--- js/index.js
+++ js/index.js
@@ -3,5 +3,18 @@
 *
 */
 'use strict'
+const inputBox = document.getElementById('inputBox')
+const result = document.getElementById('result')
 
-alert("Hello")
+inputBox.addEventListener('input', convert)
+
+function convert(e) {
+  if (map.has(e.data)) {
+    console.log(map.get(e.data))
+  }
+  result.textContent = e.target.value
+}
+
+const map = new Map([
+  ['a', 'A'], ['b', 'Bz'], ['c','DDDDX'], ['d','OK']
+])
