b214eaa135d348983077ab43b59ba757ee228b97
Delfi Sehidic <ds222qe@student.lnu.se>
2020-11-06 11:06:26 +0100

    App converts output according to map

--- js/index.js
+++ js/index.js
@@ -9,11 +9,28 @@ const result = document.getElementById('result')
 inputBox.addEventListener('input', convert)
 
 function convert(e) {
+  const input = e.target.value
+  const translation = getGlagolitic(input)
   if (map.has(e.data)) {
     console.log(map.get(e.data))
   }
-  result.textContent = e.target.value
+  result.textContent = translation
 }
+
+function getGlagolitic(input) {
+  console.log(input)
+  let newString = ""
+  for (let i = 0; i < input.length; i++) {
+    if (map.has(input.charAt(i))) {
+      newString += map.get(input.charAt(i))
+
+    } else {
+      newString += input.charAt(i)
+    }
+  }
+  return newString
+}
+
 
 const map = new Map([
   ['a', 'A'], ['b', 'Bz'], ['c','DDDDX'], ['d','OK']
