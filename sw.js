1ae7be045dedeceeb86681ebf4479ebb756fc44b
Delfi Sehidic <ds222qe@student.lnu.se>
2020-11-06 23:04:21 +0100

    Add activate event listener for sw

--- sw.js
+++ sw.js
@@ -10,3 +10,15 @@ self.addEventListener('install', (e) => {
     })
   )
 })
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((response) => {
+        return response || fetch(e.request)
+      })
+    )
+})
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim()
+})
